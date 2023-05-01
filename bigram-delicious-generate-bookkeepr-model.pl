d1ccc38913f75ca71bfda2358da3cf46474c3dd5
Tim Brown <timb@Tims-MacBook-Air.local>
2023-05-01 14:29:40 +0100

    Tidied code Reordered start-up logic Improved error handling Enabled taint mode

--- bigram-delicious-generate-bookkeepr-model.pl
+++ bigram-delicious-generate-bookkeepr-model.pl
@@ -1,12 +1,13 @@
-#!/opt/pkg/bin/perl -w
+#!/opt/pkg/bin/perl -w -T
 
+use strict;
 use JSON;
 
 my $filehandle;
 my $jsonstring;
 my $jsonobject;
 my $entry;
-my $tag;
+my $entrytag;
 my $sanitizedtag;
 my $sanitizedtitle;
 my $sanitizeddescription;
@@ -14,46 +15,52 @@ my @wordlist;
 my $wordcounter;
 my %model;
 my %weightings;
+my $modeltag;
+my $bigram;
 
-open($filehandle, "<" . $ARGV[0]);
-$jsonstring = "";
-while (<$filehandle>) {
-	$jsonstring .= $_;
-}
-close($filehandle);
-$jsonobject = decode_json($jsonstring);
-foreach $entry (@{$jsonobject}) {
-	foreach $tag (@{$entry->{'tags'}}) {
-		$sanitizedtag = $tag;
-		$sanitizedtag =~ s/[\/ ]//g;
-		if ($sanitizedtag ne "") {
-			$sanitizedtitle = $entry->{'title'};
-			$sanitizedtitle =~ s/[\-\+\.\?\\\(\)\[\]\{\}\|\/\$`"':;,!#]/ /g;
-			$sanitizedtitle = lc($sanitizedtitle);
-			$sanitizeddescription = $entry->{'description'};
-			$sanitizeddescription =~ s/[\-\+\.\?\\\(\)\[\]\{\}\|\/\$`"':;,!#]/ /g;
-			$sanitizeddescription = lc($sanitizeddescription);
-			@wordlist = split(/ /, $sanitizedtitle);
-			for ($wordcounter = 0; $wordcounter < @wordlist - 1; $wordcounter ++) {
-				$model{'tags'}{$sanitizedtag}{$wordlist[$wordcounter] . " " . $wordlist[$wordcounter + 1]}{'score'} += 1;
-				$weightings{$wordlist[$wordcounter] . " " .  $wordlist[$wordcounter + 1]} += 1;
-			}
-			@wordlist = split(/ /, $sanitizeddescription);
-			for ($wordcounter = 0; $wordcounter < @wordlist - 1; $wordcounter ++) {
-				$model{'tags'}{$sanitizedtag}{$wordlist[$wordcounter] . " " . $wordlist[$wordcounter + 1]}{'score'} += 1;
-				$weightings{$wordlist[$wordcounter] . " " .  $wordlist[$wordcounter + 1]} += 1;
-			}
-		}
-	}
-}
-foreach $tag (keys %{$model{'tags'}}) {
-	foreach $bigram (keys %{$model{'tags'}{$tag}}) {
-		$model{'tags'}{$tag}{$bigram}{'weight'} = $weightings{$bigram};
-	}
-}
 if (! -d $ENV{'HOME'} . "/.bookkeepr" ) {
 	mkdir($ENV{'HOME'} . "/.bookkeepr");
 }
-open($filehandle, ">" . $ENV{'HOME'} . "/.bookkeepr/model.json");
-print $filehandle to_json(\%model, {utf8 => 1, pretty => 1});
-close($filehandle);
+if (defined($ARGV[0])) {
+	if (-f $ARGV[0]) {
+		open($filehandle, "<" . $ARGV[0]);
+		$jsonstring = "";
+		while (<$filehandle>) {
+			$jsonstring .= $_;
+		}
+		close($filehandle);
+		$jsonobject = decode_json($jsonstring);
+		foreach $entry (@{$jsonobject}) {
+			foreach $entrytag (@{$entry->{'tags'}}) {
+				$sanitizedtag = $entrytag;
+				$sanitizedtag =~ s/[\/ ]//g;
+				if ($sanitizedtag ne "") {
+					$sanitizedtitle = $entry->{'title'};
+					$sanitizedtitle =~ s/[\-\+\.\?\\\(\)\[\]\{\}\|\/\$`"':;,!#]/ /g;
+					$sanitizedtitle = lc($sanitizedtitle);
+					$sanitizeddescription = $entry->{'description'};
+					$sanitizeddescription =~ s/[\-\+\.\?\\\(\)\[\]\{\}\|\/\$`"':;,!#]/ /g;
+					$sanitizeddescription = lc($sanitizeddescription);
+					@wordlist = split(/ /, $sanitizedtitle);
+					for ($wordcounter = 0; $wordcounter < @wordlist - 1; $wordcounter ++) {
+						$model{'tags'}{$sanitizedtag}{$wordlist[$wordcounter] . " " . $wordlist[$wordcounter + 1]}{'score'} += 1;
+						$weightings{$wordlist[$wordcounter] . " " .  $wordlist[$wordcounter + 1]} += 1;
+					}
+					@wordlist = split(/ /, $sanitizeddescription);
+					for ($wordcounter = 0; $wordcounter < @wordlist - 1; $wordcounter ++) {
+						$model{'tags'}{$sanitizedtag}{$wordlist[$wordcounter] . " " . $wordlist[$wordcounter + 1]}{'score'} += 1;
+						$weightings{$wordlist[$wordcounter] . " " .  $wordlist[$wordcounter + 1]} += 1;
+					}
+				}
+			}
+		}
+		foreach $modeltag (keys %{$model{'tags'}}) {
+			foreach $bigram (keys %{$model{'tags'}{$tag}}) {
+				$model{'tags'}{$tag}{$bigram}{'weight'} = $weightings{$bigram};
+			}
+		}
+		open($filehandle, ">" . $ENV{'HOME'} . "/.bookkeepr/model.json");
+		print $filehandle to_json(\%model, {utf8 => 1, pretty => 1});
+		close($filehandle);
+	}
+}
